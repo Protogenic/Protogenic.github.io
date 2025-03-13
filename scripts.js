f00771310a6d65c61eefbc81fb3319d6e243ec6d
Vladimir Bezrukov <82569672+Protogenic@users.noreply.github.com>
2025-03-13 11:33:08 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-<script>
-        function toggleTheme() {
-            document.body.classList.toggle('dark-mode');
-            if (document.body.classList.contains('dark-mode')) {
-                document.body.style.backgroundColor = '#222';
-                document.body.style.color = '#fff';
-            } else {
-                document.body.style.backgroundColor = '#f4f4f4';
-                document.body.style.color = '#333';
-            }
-        }
-    </script>
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        document.body.style.backgroundColor = '#222';
+        document.body.style.color = '#fff';
+    } else {
+        document.body.style.backgroundColor = '#f4f4f4';
+        document.body.style.color = '#333';
+    }
+}
